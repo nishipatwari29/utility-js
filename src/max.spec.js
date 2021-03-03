104df673318ff2dd5440c28bfa6bf86621363e6b
Nishi Patwari <nishi.patwari29@gmail.com>
2021-03-03 16:33:02 +0530

    corrected typo error

--- src/max.spec.js
+++ src/max.spec.js
@@ -5,10 +5,10 @@ describe('Max', () => {
     it('Max of [] is undefined', () => {
         expect(max([])).toEqual(undefined);
     });
-    it('Max of [3,6,31,2] is null', () => {
+    it('Max of [3,6,31,2] is 31', () => {
         expect(max([3,6,31,2])).toEqual(31);
     });
-    it('Max of [-3,-6,-31,-2] is null', () => {
+    it('Max of [-3,-6,-31,-2] is -2', () => {
         expect(max([-3,-6,-31,-2])).toEqual(-2);
     });
 
