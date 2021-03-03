c6c2fe83696d9592356bb3c346e153f466978eef
Nishi Patwari <nishi.patwari29@gmail.com>
2021-03-03 15:37:09 +0530

    refactored head.js to pass head[] test case

--- src/head.js
+++ src/head.js
@@ -1,4 +1,6 @@
 const head = (array) => {
-    return array[0];
+    if(array.length ==0 )
+        return null;
+    else return array[0];
 }
 module.exports = head;
--- src/head.spec.js
+++ src/head.spec.js
@@ -5,6 +5,8 @@ describe('Head', () => {
     it('Head of [1,2,3] is 1', () => {
         expect(head([1,2,3])).toEqual(1);
     });
-
+    it('Head of [] is null', () => {
+        expect(head([])).toEqual(null);
+    });
 
 })
